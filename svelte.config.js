237654116f612e071089a48c8c43d5bb0bdfbf75
KnutEinar <knut.einar.skaar@gmail.com>
2025-01-02 19:14:53 +0100

    change github workflow

--- svelte.config.js
+++ svelte.config.js
@@ -12,12 +12,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({ fallback : 'index.html',
-							trailingSlash: 'never'
 		 }),
-		appDir: 'app',
-		prerender: {
-			entries: ['/stones/sunnhordaland_museum', '/stones/israelsneset', '/stones/per_persasteinen', '/stones/kolagarden' ]
-		}
+		base: dev ? '' : process.env.BASE_PATH,
 	}
 };
 
