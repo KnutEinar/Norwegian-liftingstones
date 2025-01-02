6a1f14d201b5dd914b621523df8192d5db4ec2be
KnutEinar <knut.einar.skaar@gmail.com>
2025-01-02 04:31:50 +0100

    fixed file tracking

--- src/routes/(map)/+page.svelte
+++ src/routes/(map)/+page.svelte
@@ -2,13 +2,6 @@
     import Map from '$lib/Map.svelte'
 </script>
 
-<!--
-<svelte:head>
-	<script defer async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCl5xwKj0_wdWb8juJlNpfazdR6dDuKMLQ&callback=init_map"></script>
-</svelte:head>
--->
-
-
 <div class="flex h-full w-full">    
     <Map/>
 </div>
