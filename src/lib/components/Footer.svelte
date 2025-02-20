0cb027cc33d1bbf5dfaaf1b9d45be3951a7e6984
KnutEinar <knut.einar.skaar@gmail.com>
2025-02-20 22:17:52 +0100

    IG logo in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -8,7 +8,12 @@
 	<div class="bg-bg-gray h-28 flex flex-row content-center gap-6">
 		<div class="text-menu-text flex-shrink-0 px-4 content-center mx-auto">
 			<strong>{m.developed_by()}:</strong> <br />
-			Knut Einar Skår
+			<div class="flex flex-row gap-1">
+				<a class="hover:underline" href="https://www.instagram.com/knuteinarskar">
+					Knut Einar Skår
+				</a>
+				<img class="h-5 my-auto" src="/ig.png" alt="Instagram logo">
+			</div>
 		</div>
 		<div class="mx-auto my-auto">
 			<LangSelect />
