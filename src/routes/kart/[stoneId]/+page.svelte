0ff94ac0380dc953c51b989a2318e5341fd3180e
KnutEinar <knut.einar.skaar@gmail.com>
2025-02-26 12:30:59 +0100

    Fixed type error in maps/stoneId

--- src/routes/kart/[stoneId]/+page.svelte
+++ src/routes/kart/[stoneId]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Map from '$lib/components/Map.svelte';
-	import type { PageData } from '../$types';
+	import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
 </script>
