7762eb2141c9034dee1d72d2af7e25951cae6cd4
Knut Einar Skår <knut.einar.skaar@gmail.com>
2025-04-03 15:47:23 +0200

    Added logo to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,17 +5,20 @@
 </script>
 
 <footer class="mt-auto">
-	<div class="bg-bg-gray h-28 flex flex-row content-center gap-6">
-		<div class="text-menu-text flex-shrink-0 px-4 content-center mx-auto">
-			<strong>{m.developed_by()}:</strong> <br />
-			<div class="">
-				<a
-					class="hover:underline flex flex-row gap-1"
-					href="https://www.instagram.com/knuteinarskar"
-				>
-					Knut Einar Skår
-					<img class="h-5 my-auto" src="/ig.png" alt="Instagram logo" />
-				</a>
+	<div class=" bg-bg-gray h-28 flex flex-row content-center gap-6">
+		<div class="text-menu-text flex flex-shrink-0 gap-4 px-4 content-center mx-auto my-auto">
+			<img class="object-scale-down h-8 my-auto mx-auto" src="/light_logo.svg" alt="" />
+			<div>
+				<strong>{m.developed_by()}:</strong> <br />
+				<div class="">
+					<a
+						class="hover:underline flex flex-row gap-1"
+						href="https://www.instagram.com/knuteinarskar"
+					>
+						Knut Einar Skår
+						<img class="h-5 my-auto" src="/ig.png" alt="Instagram logo" />
+					</a>
+			</div>
 			</div>
 		</div>
 		<div class="mx-auto my-auto">
